139149a2875bf1e7aa87ecc66676e235bafd7c93
ktrngrdnv <ktrngrdnv@gmail.com>
2022-03-31 20:56:33 +0300

    del files

--- lesson_03.6.py
+++
@@ -1,6 +0,0 @@
-# Реализовать функцию int_func(), принимающую слова из маленьких латинских букв и возвращающую их же,
-# но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
-
-int_func = lambda string: string[0].upper() + string[1:].lower()
-
-print(int_func("TesTIng test testik ttt"))
